5d5f859cb9f5966cd1e9bd08b625360c60c74ecb
johnny larner <james.richardson.2556@gmail.com>
2021-12-11 12:18:26 +0100

    first version of script complete

--- etl.py
+++ etl.py
@@ -3,51 +3,148 @@ import glob
 import psycopg2
 import pandas as pd
 from sql_queries import *
+from env_settings import get_env_vars
 
 
 def process_song_file(cur, filepath):
-    # open song file
-    df = 
+    """
+    Reads json file as dataframe and inserts into song and artists tables.
 
-    # insert song record
-    song_data = 
-    cur.execute(song_table_insert, song_data)
+    Args:
+    - cur: psycopy2 cursor object with connection to sparkifydb.
+    - filepath: str leading to json song file.
+
+    Raises:
+    - KeyError if mandatory columns are missing from json data.
+    """
+
+    song_cols = [
+        "song_id",
+        "title",
+        "artist_id",
+        "year",
+        "duration"
+    ]
+
+    artist_cols = [
+        "artist_id",
+        "artist_name",
+        "artist_location",
+        "artist_latitude",
+        "artist_longitude"
+    ]
+
+    # open song file
+    df = pd.read_json(filepath, lines=True)
+
+    # Extract first row and export as list
+    try:
+        np_song_data = df[song_cols].iloc[0].to_numpy()
+
+    except KeyError as e: # If json is missing mandatory col
+        print("Song json missing column")
+        print(e)
+        return None
     
+    # Convert numpy data types to native ones
+    py_song_data = []
+    for np_val in np_song_data:
+        try:
+            py_val = np_val.item() # https://stackoverflow.com/questions/9452775/converting-numpy-dtypes-to-native-python-types/11389998#11389998
+        except AttributeError: # if native type, do nothing
+            py_val = np_val
+
+        py_song_data.append(py_val)
+
+
+    cur.execute(song_table_insert, py_song_data)
+
     # insert artist record
-    artist_data = 
+    try:
+        artist_data = df[artist_cols].iloc[0].to_numpy().tolist()
+
+    except KeyError as e:
+        print("Song json missing column")
+        print(e)
+        return None
+
     cur.execute(artist_table_insert, artist_data)
+
+def clean_song_year(cur):
+    """
+    Nullifies year for songs with year less than 1000.
+
+    Args:
+    - cur: psycopy2 cursor object with connection to sparkifydb.
+    """
+
+    # Select & nullify song years
+
+    cur.execute(nullify_song_year)
+
+    updated_rows = cur.rowcount
+    print("{} rows had their song years set to NULL.".format(updated_rows))
 
 
 def process_log_file(cur, filepath):
+    """
+    
+    """
+
+
+
     # open log file
-    df = 
+    df = pd.read_json(filepath, lines=True)
 
     # filter by NextSong action
-    df = 
+    df = df.loc[df["page"] == "NextSong"]
 
-    # convert timestamp column to datetime
-    t = 
-    
+    # Add date parts to DF
+    df["sng_start_time"] = pd.to_datetime(df["ts"], unit="ms", yearfirst=True)
+    df["hour"] = df["sng_start_time"].dt.hour
+    df["day"] = df["sng_start_time"].dt.day
+    df["week"] = df["sng_start_time"].dt.isocalendar().week.astype(int, errors="raise") # dt.week depreciated
+    df["month"] = df["sng_start_time"].dt.month
+    df["weekday"] = df["sng_start_time"].dt.weekday
+
     # insert time data records
-    time_data = 
-    column_labels = 
-    time_df = 
+    time_cols = [
+        "sng_start_time",
+        "hour",
+        "day",
+        "week",
+        "month",
+        "weekday"
+    ]
 
-    for i, row in time_df.iterrows():
-        cur.execute(time_table_insert, list(row))
+    # Filter to time cols
+    time_df = df[time_cols].loc[:]
+
+    for row in time_df.itertuples(index=False, name=None):
+        cur.execute(time_table_insert, row)
 
     # load user table
-    user_df = 
+    user_cols = [
+        "userId",
+        "firstName",
+        "lastName",
+        "gender",
+        "level"
+    ]
+
+    user_df = df[user_cols].loc[:]
 
     # insert user records
-    for i, row in user_df.iterrows():
+    for row in user_df.itertuples(index=False):
         cur.execute(user_table_insert, row)
 
+
     # insert songplay records
-    for index, row in df.iterrows():
-        
+    for row in df.itertuples(index=False):
+
         # get songid and artistid from song and artist tables
         cur.execute(song_select, (row.song, row.artist, row.length))
+
         results = cur.fetchone()
         
         if results:
@@ -56,7 +153,17 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = 
+        songplay_data = (
+            row.sng_start_time, 
+            row.userId, 
+            row.level, 
+            songid, 
+            artistid, 
+            row.sessionId,
+            row.location,
+            row.userAgent
+            )
+
         cur.execute(songplay_table_insert, songplay_data)
 
 
@@ -80,10 +187,17 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+
+    env_vars = get_env_vars()
+    user = env_vars["DB_USER"]
+    password = env_vars["DB_PASSWORD"]
+
+    conn = psycopg2.connect(f"host=127.0.0.1 dbname=sparkifydb user={user} password={password}")
     cur = conn.cursor()
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+    clean_song_year(cur)
+
     process_data(cur, conn, filepath='data/log_data', func=process_log_file)
 
     conn.close()
--- sql_queries.py
+++ sql_queries.py
@@ -1,49 +1,165 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id int GENERATED ALWAYS AS IDENTITY,
+    start_time timestamp,
+    user_id int,
+    level varchar(4),
+    song_id varchar(18), 
+    artist_id varchar(18),
+    session_id int,
+    location varchar,
+    user_agent varchar,
+    PRIMARY KEY(songplay_id)
+);  
 """)
 
 user_table_create = ("""
+
+CREATE TABLE IF NOT EXISTS users (
+    user_id int,
+    first_name varchar,
+    last_name varchar,
+    gender varchar,
+    level varchar(4),
+    PRIMARY KEY(user_id)
+);
+
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (
+    song_id varchar(18),
+    title varchar,
+    artist_id varchar(18),
+    year int,
+    duration float,
+    PRIMARY KEY(song_id)
+);
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id varchar(18),
+    name varchar,
+    location varchar,
+    latitude float,
+    longitude float,
+    PRIMARY KEY(artist_id)
+);
 """)
 
+# Maybe add FK here
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (
+    start_time timestamp,
+    hour int,
+    day int,
+    week int,
+    month int,
+    weekday varchar,
+    PRIMARY KEY(start_time)
+);
+
 """)
 
 # INSERT RECORDS
 
 songplay_table_insert = ("""
+INSERT INTO songplays (
+    start_time,
+    user_id,
+    level,
+    song_id,
+    artist_id,
+    session_id,
+    location,
+    user_agent
+)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT
+DO NOTHING;
 """)
 
 user_table_insert = ("""
+INSERT INTO users (
+    user_id,
+    first_name,
+    last_name,
+    gender,
+    level
+)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id)
+DO NOTHING;
 """)
 
 song_table_insert = ("""
+INSERT INTO songs (
+    song_id,
+    title,
+    artist_id,
+    year,
+    duration
+)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (song_id)
+DO NOTHING;
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists (
+    artist_id,
+    name,
+    location,
+    latitude,
+    longitude
+)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (artist_id)
+DO NOTHING;
 """)
 
 
 time_table_insert = ("""
+INSERT INTO time (
+    start_time,
+    hour,
+    day,
+    week,
+    month,
+    weekday
+)
+VALUES (%s, %s, %s, %s, %s, %s)
+ON CONFLICT (start_time)
+DO NOTHING;
 """)
 
 # FIND SONGS
 
 song_select = ("""
+SELECT s.song_id, a.artist_id
+FROM songs s
+LEFT JOIN artists a
+ON s.artist_id = a.artist_id
+WHERE s.title = %s AND a.name = %s AND s.duration = %s
+"""
+)
+
+# SELECT DATA FOR CLEANING
+
+nullify_song_year = ("""
+UPDATE songs SET year = NULL
+WHERE year < 1000;
 """)
 
 # QUERY LISTS
